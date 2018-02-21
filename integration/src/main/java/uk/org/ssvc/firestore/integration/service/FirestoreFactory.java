100ccd2dfde8173fe5391ec75f2a04104db0f112
Ed Slocombe <ed.slocombe@googlemail.com>
2018-02-21 19:16:40 +0000

    Support env vars / properties to build Google credentials

--- integration/src/main/java/uk/org/ssvc/firestore/integration/service/FirestoreFactory.java
+++ integration/src/main/java/uk/org/ssvc/firestore/integration/service/FirestoreFactory.java
@@ -1,6 +1,6 @@
 package uk.org.ssvc.firestore.integration.service;
 
-import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -8,20 +8,38 @@ import com.google.firebase.cloud.FirestoreClient;
 import uk.org.ssvc.core.domain.exception.SsvcExternalServiceException;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 @Singleton
 public class FirestoreFactory {
 
+    private String clientId;
+    private String clientEmail;
+    private String privateKey;
+    private String privateKeyId;
+    private String projectId;
+
     @Inject
-    public FirestoreFactory() {
+    public FirestoreFactory(@Named("google.clientId") String clientId,
+                            @Named("google.clientEmail") String clientEmail,
+                            @Named("google.privateKey") String privateKey,
+                            @Named("google.privateKeyId") String privateKeyId,
+                            @Named("google.projectId") String projectId) {
+        this.clientId = clientId;
+        this.clientEmail = clientEmail;
+        this.privateKey = privateKey;
+        this.privateKeyId = privateKeyId;
+        this.projectId = projectId;
     }
 
     public Firestore create() {
         try {
-            GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+            ServiceAccountCredentials credentials = ServiceAccountCredentials.fromPkcs8(
+                clientId, clientEmail, privateKey, privateKeyId, null, null, null);
             FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(credentials)
+                .setProjectId(projectId)
                 .build();
             FirebaseApp.initializeApp(options);
 
